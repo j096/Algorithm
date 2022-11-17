0768671604be532f2b70286f56923804a1ff33ca
j096 <better_daum@naver.com>
2022-11-18 00:16:49 +0900

    DP 공부

---
+++ src/dynamicPrograming/LongestIncreasingSubsequence.java
@@ -0,0 +1,46 @@
+package dynamicPrograming;
+
+import java.util.Scanner;
+
+public class LongestIncreasingSubsequence {
+	static int[] nums;
+	static int[] dy;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+		nums = new int[n+1];
+		dy = new int[n+1];
+		for(int i=1;i<=n;i++) {
+			nums[i]=s.nextInt();
+		}
+		dy[1]=1;
+		for(int i=2;i<=n;i++) {
+			int max =0;
+			for(int j=i-1;j>=1;j--) {
+				if(nums[j]<nums[i] && dy[j]>max) {
+					max=dy[j];
+				}
+			}
+			dy[i]= max+1;
+		}
+		
+		System.out.println(max(dy));
+		
+		
+	}
+	
+	public static int max(int[] dy) {
+		int max = 0;
+		for(int i=1;i<dy.length;i++) {
+			if(dy[i]>max)
+				max = dy[i];
+		}
+		
+		return max;
+	}
+	
+
+}
---
+++ src/dynamicPrograming/WalkingStairsTopDown.java
@@ -0,0 +1,29 @@
+package dynamicPrograming;
+
+import java.util.Scanner;
+
+public class WalkingStairsTopDown {
+	static int dy[];
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		dy =new int[n+1];
+		
+		System.out.println(DFS(n));
+	}
+	
+	static int DFS(int i) {
+		if(dy[i]>0)
+			return dy[i];
+		if(i == 1 || i==2) {
+			return i;
+		}
+		else {
+			dy[i]=DFS(i-1)+DFS(i-2);
+			return dy[i];
+		}
+	}
+
+}
