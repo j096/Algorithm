071faafe2718fb0d51f94b3a5b40fa2dc954cedb
j096 <better_daum@naver.com>
2022-11-24 00:25:39 +0900

    Dynamic Programing

---
+++ src/dynamicPrograming/AlibabaBottomUp.java
@@ -0,0 +1,50 @@
+package dynamicPrograming;
+
+import java.util.Scanner;
+
+public class AlibabaBottomUp {
+	static int[][] arr ;
+	static int[][] dy;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		arr = new int[n][n];
+		dy = new int[n][n];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				arr[i][j]=s.nextInt();
+			}
+		}
+		
+		dy[0][0]=arr[0][0];
+		for(int i=1;i<n;i++) {
+			dy[i][0]= dy[i-1][0]+arr[i][0];
+			dy[0][i]= dy[0][i-1]+arr[0][i];
+		}
+		for(int i=1;i<n;i++) {
+			for(int j=1;j<n;j++) {
+					dy[i][j]=min(dy[i][j-1],dy[i-1][j])+arr[i][j];
+					
+			}
+		}
+		
+		
+		System.out.println(dy[n-1][n-1]);
+		
+		
+		
+		
+
+	}
+	
+	public static int min(int a,int b) {
+		
+		if(a<b)
+			return a;
+		else
+			return b;
+	}
+
+}
---
+++ src/dynamicPrograming/GetMaxWireConnection.java
@@ -0,0 +1,45 @@
+package dynamicPrograming;
+
+import java.util.Scanner;
+
+public class GetMaxWireConnection {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//부분증가수열구하는 문제
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int[] nums = new int[n+1];
+		
+		for(int i=1;i<=n;i++) {
+			nums[i]=s.nextInt();
+		}
+		int[] dy = new int[n+1];
+		dy[1]=1;
+		for(int i=2;i<=n;i++) {
+			int max =0;
+			for(int j=i-1;j>=1;j--) {
+				if(nums[j]<nums[i] && dy[j]>max) {
+					max=dy[j];
+				}
+			}
+			dy[i]= max+1;
+		}
+		
+		System.out.println(max(dy));
+		
+		
+	}
+	
+	public static int max(int[] dy) {
+		int max = 0;
+		for(int i=1;i<dy.length;i++) {
+			if(dy[i]>max)
+				max = dy[i];
+		}
+		
+		return max;
+	}
+	
+
+}
